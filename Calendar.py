1c7084b068933d69a0832c90c6e56c2aeb1daa96
Jonathan Olsson <jonathan.olsson8@gmail.com>
2024-08-21 15:44:00 +0200

    clean slate, gör om från main med months i åtanke

--- Calendar.py
+++ Calendar.py
@@ -4,52 +4,7 @@ from tabulate import tabulate
 class Calendar():
 
     def __init__(self, start_month, end_month):
-
-        m1 = start_month
-        m2 = end_month
-
-        self.range = np.arange(m1,m2,1)
-        self.calendar = []
-
-        for month in self.range:
-            if month in [0,2,4,6,7,9,11]:
-                list = np.arange(1,31+1,1)
-                list = list.tolist()
-                self.calendar.append(list)
-            if month in [3,5,8,10]:
-                list = np.arange(1,30+1,1)
-                list = list.tolist()
-                self.calendar.append(list)
-            if month == 1:
-                list = np.arange(1,28+1,1)
-                list = list.tolist()
-                self.calendar.append(list)
+        pass
 
     def getCalendar(self):
-        table = []
-        for idx in np.arange(0,len(self.range),1):
-            if self.range[idx] == 0:
-                table.append(['January', str(self.calendar[idx])])
-            if self.range[idx] == 1:
-                table.append(['Februari', str(self.calendar[idx])])
-            if self.range[idx] == 2:
-                table.append(['Mars', str(self.calendar[idx])])
-            if self.range[idx] == 3:
-                table.append(['April', str(self.calendar[idx])])
-            if self.range[idx] == 4:
-                table.append(['Maj', str(self.calendar[idx])])
-            if self.range[idx] == 5:
-                table.append(['Juni', str(self.calendar[idx])])
-            if self.range[idx] == 6:
-                table.append(['Juli', str(self.calendar[idx])])
-            if self.range[idx] == 7:
-                table.append(['Augusti', str(self.calendar[idx])])
-            if self.range[idx] == 8:
-                table.append(['September', str(self.calendar[idx])])
-            if self.range[idx] == 9:
-                table.append(['Oktober', str(self.calendar[idx])])
-            if self.range[idx] == 10:
-                table.append(['November', str(self.calendar[idx])])
-            if self.range[idx] == 11:
-                table.append(['December', str(self.calendar[idx])])
-        return tabulate(table)
+        pass
--- person.py
+++ person.py
@@ -3,39 +3,19 @@ import numpy as np
 class Person:
 
     def __init__(self, name, missing_dates):
-        self.name = name
-        self.missing_dates = missing_dates
-        self.cleaning_dates = []
-        self.rested = True
-        self.cleaning_streak = 0
-        self.rest_days = 0
+        pass
     
     def addMissing(self, date):
-        self.missing_dates.append(date)
-        self.calibrateMissingDates()
+        pass
 
     def calibrateMissingDates(self):
-        idx = 0
-        while idx < len(self.missing_dates):
-            if idx + 1 == len(self.missing_dates):
-                break
-            diff = self.missing_dates[idx+1] - self.missing_dates[idx]
-            if diff < 3:
-                missing_dates = np.arange(self.missing_dates[idx]+1, self.missing_dates[idx+1], 1)
-                missing_dates = missing_dates.tolist()
-                self.missing_dates = self.missing_dates[0:idx+1] + missing_dates + self.missing_dates[idx+1:len(self.missing_dates)]
-            idx += 1
+        pass
     
     def getTotalCleaningDays(self):
-        sum = 0
-        for day in self.cleaning_dates:
-            if day == 'X':
-                sum += 1
-        return sum
+        pass
 
     def __str__(self):
-        string = str(self.name) + ' - ' + str(self.missing_dates)
-        return string
+        pass
 
 if __name__ == '__main__':
     pass
